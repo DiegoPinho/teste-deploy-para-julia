c7467acc9903f62ba2768bfc4aa3512d0090efc0
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-09-06 21:22:48 -0300

    rename jellyfish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,58 +4,58 @@ import Nav from '@/components/Nav';
 import astronauta from "@/assets/astronalta.png";
 import { StarBackground } from '@/components/StarBackground';
 import { Contact } from '@/components/Contact';
-import jellyfish from "@/assets/Jellyfish.png"
+import Jellyfish from "@/assets/Jellyfish.png"
 import { Typewriter } from 'react-simple-typewriter';
 import { useTheme } from '@/contexts/ThemeContext';
 import BubbleBackground from '../components/bubblesBackground';
 
 
 function Home() {
-  const {theme}=useTheme();
+  const { theme } = useTheme();
 
   return (
 
 
     <div className="app min-h-screen bg-background text-foreground overflow-x-hidden">
-     
-    {
-      theme === "light" ? <BubbleBackground/> : <StarBackground/>
-    }
-    
-    <Header> </Header>
-    <Nav></Nav>
 
-    <main className="intro-section">
+      {
+        theme === "light" ? <BubbleBackground /> : <StarBackground />
+      }
+
+      <Header> </Header>
+      <Nav></Nav>
+
+      <main className="intro-section">
         <div className="intro-text">
           <h1>Hi, I'm <span className="highlight">Julia</span> Becho</h1>
-          <h2>Full-Stack Developer with a Passion for <span className='highlight'> 
+          <h2>Full-Stack Developer with a Passion for <span className='highlight'>
             <Typewriter
-            words={[
-              "UX/UI desing",
-              "Front-end Development",
-              "Web performace"
-            ]}
-            loop={0}
-            cursor
-            cursorStyle='|'
-            typeSpeed={80}
-            deleteSpeed={50}
-            delaySpeed={1500}
+              words={[
+                "UX/UI desing",
+                "Front-end Development",
+                "Web performace"
+              ]}
+              loop={0}
+              cursor
+              cursorStyle='|'
+              typeSpeed={80}
+              deleteSpeed={50}
+              delaySpeed={1500}
 
             />
-          
-            </span></h2>
+
+          </span></h2>
         </div>
-            {
-              theme === "dark" ? (
-                <img src={astronauta} alt="Astronauta" className="hero-img" />
-              ) : (
-                <img src={jellyfish} alt="jellyfish" className="hero-img light" />
-              )
-            }
+        {
+          theme === "dark" ? (
+            <img src={astronauta} alt="Astronauta" className="hero-img" />
+          ) : (
+            <img src={Jellyfish} alt="jellyfish" className="hero-img light" />
+          )
+        }
       </main>
 
-     <Contact></Contact>
+      <Contact></Contact>
 
     </div>
   );
